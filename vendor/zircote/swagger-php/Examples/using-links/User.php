731222856a8448062ca4686a4cc6d562ad951962
tejaswimvm <tejaswi.mvmbs@gmail.com>
2025-10-01 14:01:38 +0530

    Updates on files

---
+++ vendor/zircote/swagger-php/Examples/using-links/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace OpenApi\Examples\OpenapiSpec;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Schema(schema="user")
+ */
+class User
+{
+
+    /**
+     * @OA\Property
+     *
+     * @var string
+     */
+    public $username;
+
+    /**
+     * @OA\Property
+     *
+     * @var string
+     */
+    public $uuid;
+}
